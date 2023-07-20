9f6a371e9a528f2eb95fb803c13fce8b710192d3
Hilal Fakherdine <hilalfeld@gmail.com>
2023-07-20 19:51:51 +0300

    commit-20-7-23

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 var semesterCount = 0;
 addSemester();
 // var calculateButton = document.getElementById('calculate');
-function calculateGPA() {
-}
+
 function addSemester() {
     semesterCount++;
     let semesterContainer = document.getElementById("semester-container");
@@ -34,19 +33,26 @@ function addCourse(semesterDiv) {
 
     var courseCredit = document.createElement("input");
     courseCredit.type = "number";
+    courseCredit.className = "credits";
     newCourse.appendChild(courseCredit);
 
     var gradesSelectInput = document.createElement("select");
     var initialOption = document.createElement("option");
+    gradesSelectInput.className = "grades";
     initialOption.text = "Select Grade";
     initialOption.disabled = true;
     initialOption.selected = true;
     gradesSelectInput.appendChild(initialOption);
 
     var removeCourse = document.createElement("button");
+    // removeCourse.innerHTML = "remove";
+    let crossIcon = document.createElement("i");
+    crossIcon.classList = "lni lni-cross-circle";
+    removeCourse.appendChild(crossIcon);
     removeCourse.onclick = function () {
         removeCourseFunction(newCourse);
     };
+    newCourse.appendChild(removeCourse);
 
     var grades = [
         "A+",
@@ -85,4 +91,61 @@ function removeSemester(semester) {
     semester.remove();
     semesterCount--;
     // if (semesterCount === 0) calculateButton.disabled = true;
+}
+
+function gradeToNumber(grade) {
+    switch (grade) {
+        case "A+":
+            return 4.0;
+        case "A":
+            return 4.0;
+        case "A-":
+            return 3.7;
+        case "B+":
+            return 3.3;
+        case "B":
+            return 3.0;
+        case "B-":
+            return 2.7;
+        case "C+":
+            return 2.3;
+        case "C":
+            return 2.0;
+        case "C-":
+            return 1.7;
+        case "D+":
+            return 1.3;
+        case "D":
+            return 1.0;
+        case "D-":
+            return 0.7;
+        case "F":
+            return 0.0;
+        default:
+            return NaN;
+    }
+}
+
+function calculateGPA() {
+
+    var totalGrades = 0;
+    var totalCredits = 0;
+
+    for (var i = 0; i < semesterCount; i++) {
+        // let courseCounter = document.getElementsByClassName("course");
+        let creditsElement = document.getElementsByClassName("credits");
+        // console.log(creditsElement[0].value);
+        let gradesElement = document.getElementsByClassName("grades");
+        // console.log(gradesElement[0].value);
+        for (var j = 0; j < creditsElement.length; j++) {
+            var credit = creditsElement[j].value;
+            var grade = gradeToNumber(gradesElement[j].value);
+            // console.log(gradesElement[0].value);
+            if (!credit || isNaN(credit) || isNaN(grade)) {
+                alert("Please fill out everything");
+            }
+            console.log(credit + " type is " + typeof (credit));
+            console.log(grade + " type is " + typeof (grade));
+        }
+    }
 }
